5487df5d5893d20de281e15afd633d58a128eff8
sorethea <sorethea@gmail.com>
2026-08-03 10:00:51 +0700

    add localizations

--- app/Filament/Pages/VehicleSettingPage.php
+++ app/Filament/Pages/VehicleSettingPage.php
@@ -3,14 +3,18 @@
 namespace App\Filament\Pages;
 
 use App\Settings\VehicleSetting;
+use BackedEnum;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
+use Filament\Support\Icons\Heroicon;
 use VanOns\FilamentSettings\Filament\Pages\SettingsPage;
 
 class VehicleSettingPage extends SettingsPage
 {
     protected static ?string $title = 'My Vehicle';
     protected string $settingsClass = VehicleSetting::class;
+
+    protected static string | BackedEnum | null $navigationIcon = Heroicon::Truck;
 
     public function getFormSchema(): array
     {
